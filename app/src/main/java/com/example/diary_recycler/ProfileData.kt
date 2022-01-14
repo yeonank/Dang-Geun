84bccbc5891e3157824edf6a79597fba6ad4344b
HSHyeon <hshyeon86@gmail.com>
2022-01-15 01:14:53 +0900

    INSERT

---
+++ app/src/main/java/com/example/diary_recycler/ProfileData.kt
@@ -0,0 +1,4 @@
+package com.example.diary_recycler
+
+class ProfileData {
+}
---
+++ app/src/main/java/com/example/diary_recycler/view/HttpClient.kt
@@ -0,0 +1,21 @@
+package com.example.diary_recycler.view
+
+import com.example.diary_recycler.RetrofitApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+public class HttpClient {
+    companion object {
+        //통신할 서버 url
+        private const val baseUrl = "http://12.345.678.910"
+
+        //Retrofit 객체 초기화
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(this.baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val test: RetrofitApi = retrofit.create(RetrofitApi::class.java)
+    }
+}
