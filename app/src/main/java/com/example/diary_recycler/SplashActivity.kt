fca7192ebc7715729ef50623b739959f3c009990
HSHyeon <hshyeon86@gmail.com>
2022-02-05 23:11:33 +0900

    [UPDATE] autologin

--- app/src/main/java/com/example/diary_recycler/SplashActivity.kt
+++ app/src/main/java/com/example/diary_recycler/SplashActivity.kt
@@ -5,24 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.diary_recycler.view.activity.GoogleLoginActivity
+import com.example.diary_recycler.view.activity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
-            val intent = Intent(this, GoogleLoginActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+
+    public override fun onStart() {
+            super.onStart()
+            var auth = FirebaseAuth.getInstance()
+            moveMainPage(auth?.currentUser)
+        }
+
+    fun moveMainPage(user: FirebaseUser?){
+            Handler().postDelayed({
+            if( user!= null){
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()}
+            else
+                {
+                val intent = Intent(this, GoogleLoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()}
         },DURATION)
 
     }
     companion object {
         private const val DURATION : Long = 3000
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
+}
 }
