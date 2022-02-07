a5c16c490bb0ab6224cd0caf4513730662353e4e
HSHyeon <hshyeon86@gmail.com>
2022-02-08 00:27:38 +0900

    [UPDATE] api connect(예정)

--- app/src/main/java/com/example/diary_recycler/view/activity/GoogleLoginActivity.kt
+++ app/src/main/java/com/example/diary_recycler/view/activity/GoogleLoginActivity.kt
@@ -184,12 +184,12 @@ class GoogleLoginActivity : AppCompatActivity() {
                         }
 
 
-                    }))
+                    }))  */
 
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
-            */    }
+             }
             }
     }
 
